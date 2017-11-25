40d9cf84bf17802b4b8bbeb90b8e0b65429910e2
Wolfgang <wolf@okwolf.com>
2017-11-25 13:08:26 -0800

    omitting default module wrapper property from test output

--- src/flattenTests.js
+++ src/flattenTests.js
@@ -1,16 +1,18 @@
-const flattenTests = (test, prefixes = []) =>
+const flattenTests = (test = {}, prefixes = []) =>
   Object.keys(test).reduce(
     (tests, name) =>
-      Array.isArray(test[name])
-        ? tests.concat({
-            prefixes,
-            name,
-            failure: !test[name][0] ? test[name][1] : null
-          })
-        : tests.concat(
-            { name, prefixes },
-            flattenTests(test[name], prefixes.concat(name))
-          ),
+      prefixes.length === 0 && name === "default"
+        ? flattenTests(test.default)
+        : Array.isArray(test[name])
+          ? tests.concat({
+              prefixes,
+              name,
+              failure: !test[name][0] ? test[name][1] : null
+            })
+          : tests.concat(
+              { name, prefixes },
+              flattenTests(test[name], prefixes.concat(name))
+            ),
     []
   );
 
---
+++ test/flattenTests.test.js
@@ -0,0 +1,37 @@
+import flattenTests from "../src/flattenTests";
+
+export default {
+  flattenTests: {
+    "is a function": ["function", typeof flattenTests],
+    "is empty without arguments": [flattenTests(), []],
+    flattens: {
+      "empty test object to empty list": [flattenTests({}), []],
+      "single passing test object": [
+        flattenTests({ passing: [true] }),
+        [{ prefixes: [], name: "passing", failure: null }]
+      ],
+      "single failing test object": [
+        flattenTests({ failing: [false, { before: true, after: false }] }),
+        [
+          {
+            prefixes: [],
+            name: "failing",
+            failure: { before: true, after: false }
+          }
+        ]
+      ],
+      "module passing test object skips default export property": [
+        flattenTests({ default: { passing: [true] } }),
+        [{ prefixes: [], name: "passing", failure: null }]
+      ],
+      "includes nested default property": [
+        flattenTests({ top: { default: { passing: [true] } } }),
+        [
+          { prefixes: [], name: "top" },
+          { prefixes: ["top"], name: "default" },
+          { prefixes: ["top", "default"], name: "passing", failure: null }
+        ]
+      ]
+    }
+  }
+};
