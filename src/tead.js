c8e6cce53756f07d95693ef3d9beb549aff64bf3
Wolfgang <wolf@okwolf.com>
2017-12-08 23:44:16 -0800

    initial attempt at coverage flag

--- src/tead.js
+++ src/tead.js
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
 
-require = require("@std/esm")(module, {
-  esm: "js",
-  cjs: true
-});
-require("./")(require("./options")());
+const options = require("./options")();
+
+if (!options.noesm) {
+  require = require("@std/esm")(module, {
+    esm: "js",
+    cjs: true
+  });
+}
+
+require("./")(options);
