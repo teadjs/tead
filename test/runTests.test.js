dc49d906a91401b2d84fa3e9d6331692dd1112f7
Wolfgang <wolf@okwolf.com>
2018-01-13 23:16:27 -0800

    coverage for runTests

---
+++ test/runTests.test.js
@@ -0,0 +1,50 @@
+import runTests from "../src/runTests";
+
+export default {
+  runTests: {
+    "is a function": [typeof runTests, "function"],
+    "is invalid for": {
+      "missing arguments": [
+        runTests(),
+        { "invalid test": [false, [{ invalid: undefined }]] }
+      ],
+      true: [runTests(true), { "invalid test": [false, [{ invalid: true }]] }],
+      numbers: [runTests(666), { "invalid test": [false, [{ invalid: 666 }]] }],
+      strings: [
+        runTests("foobar"),
+        { "invalid test": [false, [{ invalid: "foobar" }]] }
+      ]
+    },
+    runs: {
+      "passing test": [runTests([true, true]), [true, []]],
+      "failing test": [
+        runTests([false, true]),
+        [false, [{ before: true, after: false, context: [] }]]
+      ],
+      "nested tests": [
+        runTests({
+          multiple: {
+            levels: {
+              of: { tests: { passing: [true, true], failing: [false, true] } }
+            }
+          }
+        }),
+        {
+          multiple: {
+            levels: {
+              of: {
+                tests: {
+                  passing: [true, []],
+                  failing: [
+                    false,
+                    [{ before: true, after: false, context: [] }]
+                  ]
+                }
+              }
+            }
+          }
+        }
+      ]
+    }
+  }
+};
